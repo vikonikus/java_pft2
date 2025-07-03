3ef5fe1d4abc5eddf57393931f1ca7ce9b2cc9d2
vikonikus <vikonik@mail.ru>
2025-07-03 22:32:44 +0400

    Добавление теста с использованием TestNG для класса Square.

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -5,17 +5,17 @@ public class MyFirstProgram {
 	public static void main (String[] args){
 
 		Square s = new Square(5);
-		System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+		System.out.println("Square's area " + s.l + " = " + s.area());
 
 		Rectangle r = new Rectangle(2, 5);
-		System.out.println("Площадь прямоугольника со сторонами " + r.a + " и "
+		System.out.println("Rectangle's area " + r.a + " and "
 						+ r.b + " = " + r.area());
 
 
 
 		Point p1 = new Point(0, 0);
 		Point p2 = new Point(1, 1);
-		System.out.println("Расстояние между точками p1 и p2 = " + p1.distanceTo(p2));
+		System.out.println("Distance from p1 to p2 = " + p1.distanceTo(p2));
 
 	}
 
--- sandbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.sandbox;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class SquareTests {
@@ -7,6 +8,6 @@ public class SquareTests {
   @Test
   public void testArea() {
     Square s = new Square(5);
-    assert s.area() == 25.0;
+    Assert.assertEquals(s.area(), 25.0);
   }
 }
